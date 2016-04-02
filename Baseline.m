ccbfba8192e987c16cfa34c6792d3a4306137cd6
Taylor Peterson <TaylorP913@Gmail.com>
2016-04-01 22:36:12 -0600

    clean basline

--- Baseline.m
+++ Baseline.m
@@ -10,9 +10,9 @@ TraceArray = zeros(length(files), 20003);
 keyreal = [0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1];
 key = zeros(1,64);
 
-outs = zeros(length(files), 64, 8);
-iOld = 0;
-iter = 0;
+hamms = zeros(length(files), 64, 8);
+old = 0;
+cnt = 0;
 
 for Trace = 1:length(files) 
     Data = csvread(strcat('C:\Users\Taylor\Desktop\SCA Files\CSV\', files(Trace).name));
@@ -20,38 +20,32 @@ for Trace = 1:length(files)
     
     binPT = hex2bi(char(plaintexts(Trace)));
     
-    X = DESHAMM(binPT,'ENC',key);
+    hamms(Trace,:,:) = permute(DESHAMM(binPT,'ENC',key),[3,2,1]);
     
-    outs(Trace,:,:) = permute(X,[3,2,1]);
-    
-    Gcorr = zeros(8, 64, 11); 
-    I = zeros(8, 1);
+    Corrs = zeros(8, 64, 11); 
+    Subkey = zeros(8, 1);
     Ish = zeros(8, 1);
-    for mk=1:8
-        Gcorr(mk, :, :) = corr(outs(1:Trace, :, mk),TraceArray(1:Trace, 5739:5749));
-        [~,Ish(mk)] = max(max(Gcorr(mk, :, :)));
-        [~,I(mk)] = max(Gcorr(mk, :, Ish(mk)));
+    for k=1:8
+        Corrs(k, :, :) = corr(hamms(1:Trace, :, k),TraceArray(1:Trace, 5739:5749));
+        [~,Ish(k)] = max(max(Corrs(k, :, :)));
+        [~,Subkey(k)] = max(Corrs(k, :, Ish(k)));
+    end
+    disp(Subkey');
+    cnt 
+    Trace
+    if ~isequal(old,Subkey)
+        cnt = 0;
+    else
+        cnt = cnt+1;
     end
     
-        disp(I);
-        disp(iter);
-    if ~isequal(iOld, I)
-        %disp(iter);
-        iter = 0;
-    end
-    iter = iter+1;
-    %disp(iter);
-    if iter > 100
+    if cnt >= 99
         break;
     end
-    iOld = I;
-    
-    
-
+    old = Subkey;
 end
 
 
-Subkey =[57;12;60;39;1;14;26;56]; %technically it is this minus 1. The minus one is doen in the findKey function.
-Subkey = I;
+%Subkey =[57;12;60;39;1;14;26;56]; %technically it is this minus 1. The minus one is doen in the findKey function.
 hexkey = FindKey(Subkey);
 disp(hexkey);
