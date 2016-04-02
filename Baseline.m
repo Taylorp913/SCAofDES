d276ffe5ba356e73fafbc946b5afcbb60d614461
Taylor Peterson <TaylorP913@Gmail.com>
2016-04-01 21:10:01 -0600

    Revert "Baseline is working"
    This reverts commit 5e3d25eba405686c3dcb3fcd58f9c40515053cb4.

--- Baseline.m
+++ Baseline.m
@@ -9,45 +9,14 @@ TraceArray = zeros(length(files), 20003);
 keyreal = [0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1];
 key = zeros(1,64);
 
-outs = zeros(length(files), 64, 8);
-
-for Trace = 1:length(files) 
+for Trace = 1:1%length(files) 
     Data = csvread(strcat('C:\Users\Taylor\Desktop\SCA Files\CSV\', files(Trace).name));
     TraceArray(Trace,:) = Data(:,2);
     
     binPT = hex2bi(char(plaintexts(Trace)));
     
-    X = DESHAMM(binPT,'ENC',key);
+    X = DES(binPT,'ENC',keyreal); 
     
-    outs(Trace,:,:) = permute(X,[3,2,1]);
-    
-    Gcorr = zeros(8, 64, 11); 
-    I = zeros(8, 1);
-    Ish = zeros(8, 1);
-    for mk=1:8
-        Gcorr(mk, :, :) = corr(outs(1:Trace, :, mk),TraceArray(1:Trace, 5739:5749));
-        [~,Ish(mk)] = max(max(Gcorr(mk, :, :)));
-        [~,I(mk)] = max(Gcorr(mk, :, Ish(mk)));
-    end
-    
-        disp(I); 
-    if ~isequal(iOld, I)
-        disp(iter);
-        iter = 0;
-    end
-    iter = iter+1;
-    disp(iter);
-    if iter > 100
-        break;
-    end
-    iOld = I;0.
-    
-    
-
 end
 
-
-Subkey =[57;12;60;39;1;14;26;56]; %technically it is this minus 1. The minus one is doen in the findKey function.
-Subkey = I;
-hexkey = FindKey(Subkey);
-disp(hexkey);
+log2hex(X)
