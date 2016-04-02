41a83b8297bec61a4247eda4de5ff8d83e0ebd2d
Taylor Peterson <TaylorP913@Gmail.com>
2016-04-01 21:28:45 -0600

    Okay, I broke it for a little while. Now it is working again better than ever.

--- Baseline.m
+++ Baseline.m
@@ -1,4 +1,5 @@
-
+clear all;
+clc;
 
 files = dir('C:\Users\Taylor\Desktop\SCA Files\CSV\*.csv');
 plaintexts = textread('MValues.csv','%s');
@@ -9,14 +10,48 @@ TraceArray = zeros(length(files), 20003);
 keyreal = [0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1 0 1 1 1 1 0 0 0 0 1 1 0 1 0 1 0 0 1 1 0 0 1 0 1];
 key = zeros(1,64);
 
-for Trace = 1:1%length(files) 
+outs = zeros(length(files), 64, 8);
+iOld = 0;
+iter = 0;
+
+for Trace = 1:length(files) 
     Data = csvread(strcat('C:\Users\Taylor\Desktop\SCA Files\CSV\', files(Trace).name));
     TraceArray(Trace,:) = Data(:,2);
     
     binPT = hex2bi(char(plaintexts(Trace)));
     
-    X = DES(binPT,'ENC',keyreal); 
+    X = DESHAMM(binPT,'ENC',key);
     
+    outs(Trace,:,:) = permute(X,[3,2,1]);
+    
+    Gcorr = zeros(8, 64, 11); 
+    I = zeros(8, 1);
+    Ish = zeros(8, 1);
+    for mk=1:8
+        Gcorr(mk, :, :) = corr(outs(1:Trace, :, mk),TraceArray(1:Trace, 5739:5749));
+        [~,Ish(mk)] = max(max(Gcorr(mk, :, :)));
+        [~,I(mk)] = max(Gcorr(mk, :, Ish(mk)));
+    end
+    
+        disp(I);
+        disp(iter);
+    if ~isequal(iOld, I)
+        %disp(iter);
+        iter = 0;
+    end
+    iter = iter+1;
+    %disp(iter);
+    if iter > 100
+        break;
+    end
+    iOld = I;
+    
+    
+
 end
 
-log2hex(X)
+
+Subkey =[57;12;60;39;1;14;26;56]; %technically it is this minus 1. The minus one is doen in the findKey function.
+Subkey = I;
+hexkey = FindKey(Subkey);
+disp(hexkey);
